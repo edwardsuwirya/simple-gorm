478d242c4676623f068968e5ea8a38a0bb9c2db6
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-11-23 16:50:52 +0700

    one to one relationship

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"strings"
 	"time"
 )
 
@@ -23,7 +24,7 @@ func main() {
 		panic(err)
 	}
 	//newStudentRepository(db)
-
+	newInventoryRepository(db)
 }
 
 type studentRepository struct {
@@ -100,4 +101,92 @@ func (r *studentRepository) createStudent(student Student) (*Student, error) {
 func (r *studentRepository) deleteStudent(id int) error {
 	err := r.db.Delete(&Student{}, id).Error
 	return err
+}
+
+type inventoryRepository struct {
+	db *gorm.DB
+}
+
+func newInventoryRepository(db *gorm.DB) {
+	repo := new(inventoryRepository)
+	repo.db = db
+	repo.run()
+}
+func (i *inventoryRepository) run() {
+	//category, err := i.CreateCategory(Category{
+	//	CategoryName: "Sembako",
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(category.ToString())
+
+	//product, err := i.CreateProduct(Product{
+	//	ProductCode: "A0002",
+	//	ProductName: "Minyak goreng",
+	//	CategoryId:  "0e766440-6ab6-43fa-b9c2-e5211039baa8",
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(product.ToString())
+
+	//Create product plus create category
+	//product, err := i.CreateProduct(Product{
+	//	ProductCode: "A0003",
+	//	ProductName: "Sabun Batang",
+	//	Category: Category{
+	//		CategoryName: "Peralatan Mandi",
+	//	},
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(product.ToString())
+	//fmt.Printf("%s\n", strings.Repeat("=", 50))
+	//products, err := i.getProductWithCategory()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, product := range products {
+	//	fmt.Println(product.ToString())
+	//}
+
+	fmt.Printf("%s\n", strings.Repeat("=", 50))
+	categories, err := i.getCategories()
+	if err != nil {
+		panic(err)
+	}
+	for _, category := range categories {
+		fmt.Println(category.ToString())
+	}
+}
+func (i *inventoryRepository) getCategories() ([]Category, error) {
+	categories := make([]Category, 0)
+	err := i.db.Find(&categories).Error
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (i *inventoryRepository) getProductWithCategory() ([]Product, error) {
+	products := make([]Product, 0)
+	err := i.db.Preload("Category").Find(&products).Error
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+func (i *inventoryRepository) CreateCategory(category Category) (*Category, error) {
+	if err := i.db.Create(&category).Error; err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+func (i *inventoryRepository) CreateProduct(product Product) (*Product, error) {
+	if err := i.db.Create(&product).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
 }
